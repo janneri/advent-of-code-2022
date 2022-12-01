78064f41a144164543bc1c6e8e7b5a1ff82d6e0c
Janne Rintanen <janne.rintanen@solita.fi>
2022-12-01 15:36:10 +0200

    Add solution for day1.

---
+++ src/main/kotlin/day1/main.kt
@@ -0,0 +1,67 @@
+package day1
+
+import util.readNullableInts
+import java.io.File
+
+data class Reindeer(val sums: List<Int>)
+
+fun parseReindeers(lines: List<String>): List<Reindeer> {
+    var nums = mutableListOf<Int>()
+    val reindeers = mutableListOf<Reindeer>()
+
+    lines.forEach {
+        if (it.isBlank()) {
+            reindeers.add(Reindeer(nums))
+            nums = mutableListOf()
+        } else {
+            nums.add(it.toInt())
+        }
+    }
+
+    if (nums.isNotEmpty()) reindeers.add(Reindeer(nums))
+
+    return reindeers
+}
+
+fun parseReindeersFold(ints: List<Int?>): MutableList<Int> {
+    return ints.fold(mutableListOf(0)) { acc, num ->
+        if (num != null) {
+            acc[acc.lastIndex] = acc.last() + num
+        } else {
+            acc.add(0)
+        }
+        acc
+    }
+}
+
+fun part1(lines: List<String>): Int {
+    val reindeers = parseReindeers(lines)
+    val maxCaloryReindeer = reindeers.maxWith(Comparator.comparingInt { it.sums.sum() })
+    return maxCaloryReindeer.sums.sum()
+}
+
+fun part2(lines: List<String>): Int {
+    val reindeers = parseReindeers(lines)
+    val maxCaloryReindeers = reindeers.sortedWith(Comparator.comparingInt { it.sums.sum() }).reversed().take(3)
+    val calorySum = maxCaloryReindeers.map({it.sums.sum()}).sum()
+    println(maxCaloryReindeers)
+    return calorySum
+}
+
+fun part1Fold(ints: List<Int?>): Int {
+    return parseReindeersFold(ints).max()
+}
+
+fun part2Fold(ints: List<Int?>): Int {
+    return parseReindeersFold(ints).sorted().reversed().take(3).sum()
+}
+
+fun main() {
+    val inputfile = "src/main/kotlin/day1/testinput.txt"
+    println(part1Fold(readNullableInts(inputfile)))
+    println(part2Fold(readNullableInts(inputfile)))
+
+    val input = File(inputfile).readLines()
+    println(part1(input))
+    println(part2(input))
+}
