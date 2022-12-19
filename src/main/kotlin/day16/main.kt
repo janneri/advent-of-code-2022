d8dbe4198fc45e3278a84b2fbb14653221602a81
Janne Rintanen <janne.rintanen@solita.fi>
2022-12-19 22:01:05 +0200

    Day 16: implement part 2.

--- src/main/kotlin/day16/main.kt
+++ src/main/kotlin/day16/main.kt
@@ -1,208 +1,210 @@
 package day16
 
-import util.readInput
 import util.readTestInput
-import java.lang.IllegalArgumentException
+import kotlin.math.min
 
-private typealias Path = List<Valve>
+private typealias ValveId = String
 
-private data class Valve(val symbol: String, val flowrate: Int, val neighbors: List<String>) {
-    override fun toString(): String = symbol
+private data class Valve(val valveId: ValveId) {
+    var flowrate: Int = 0
+    var neighbors: List<String> = listOf()
+
+    private constructor(valveId: ValveId, flowrate: Int, neighbors: List<ValveId>): this(valveId) {
+        this.flowrate = flowrate
+        this.neighbors = neighbors
+    }
+
+    override fun toString(): String = valveId
 
     companion object {
+        // Parses input lines, such as:
+        // Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+        // Valve JJ has flow rate=21; tunnel leads to valve II
         fun parse(str: String): Valve {
-            // Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
-            // Valve JJ has flow rate=21; tunnel leads to valve II
-            val symbol = str.substring(6, 8)
+            val valveId = str.substring(6, 8)
             val neighbors = when {
                 str.contains("valves") -> str.substringAfter("to valves ").split(", ")
                 else -> str.substringAfter("to valve ").split(", ")
             }
             val flowrate = str.substringAfter("flow rate=").takeWhile { it != ';' }.toInt()
-            return Valve(symbol, flowrate, neighbors)
+            return Valve(valveId, flowrate, neighbors)
         }
     }
+
 }
 
-private data class GameState(var totalPressureReleased : Int = 0, val currentValve: Valve, val openValves: Set<Valve>) {
+private data class GameState(var totalPressureReleased : Int = 0, val currentValve: Valve,
+                             val currentElephantValve: Valve?, val openValves: Set<Valve>) {
     fun withNewTotalPressure(): GameState {
-        totalPressureReleased += openValves.map { it.flowrate }.sum()
+        totalPressureReleased += openValves.sumOf { it.flowrate }
         return this
     }
+
+    fun currentValveOpenable(): Boolean =
+        !openValves.contains(currentValve) && currentValve.flowrate > 0
+    fun currentElephantValveOpenable(): Boolean =
+        !openValves.contains(currentElephantValve) && currentElephantValve!!.flowrate > 0
+
+    fun currentPressure(): Int = openValves.sumOf { it.flowrate }
+
 }
 
-/*
-override fun hashCode() = EssentialData(this).hashCode()
-        override fun toString() : String {
-            return if(isLeaf()) leaf.toString()
-            else "[" + left?.toString() + "," + right?.toString() + "]"
+private class Tunnels(valves: List<Valve>, initialValveId: ValveId, elephantValveId: ValveId?) {
+    val valvesById: Map<ValveId, Valve> = valves.fold(mutableMapOf()) { acc, valve ->
+        acc[valve.valveId] = valve
+        acc
+    }
+    val openableValves = valves.filter { it.flowrate > 0 }
+    val openableValveCount = openableValves.size
+    // Distances from all nodes to any other node. In the sample, distances["AA"]["BB"] would be 2
+    val distances: Map<ValveId, Map<ValveId, Int>> = valves.associate { it.valveId to getDistances(it) }
+
+    val initialState = GameState(
+        currentValve = valvesById[initialValveId]!!,
+        currentElephantValve = valvesById[elephantValveId],
+        openValves = mutableSetOf()
+    )
+
+    fun getDistances(fromValve: Valve): Map<ValveId, Int> {
+        val visited = mutableSetOf<Valve>()
+        val distances = mutableMapOf(fromValve.valveId to 0)
+        val queue = mutableListOf(fromValve)
+        while (queue.isNotEmpty()) {
+            val current = queue.removeAt(0)
+            if (!visited.contains(current)) {
+                visited.add(current)
+                current.neighbors.forEach{neighbor ->
+                    val newDistance = distances[current.valveId]!! + 1
+                    if (distances[neighbor] == null) distances[neighbor] = newDistance
+                    queue.add(valvesById[neighbor]!!)
+                }
+            }
         }
+        return distances
     }
 
-private data class EssentialData(val currentValve: Valve, val openValves: Set<Valve>) {
-    constructor(state: GameState) : this(leaf = number.leaf)
-}
+    fun theoreticalMax(gameState: GameState, timeLeft: Int): Int {
+        var closedValveSum = 0
+        if (timeLeft > 1) {
+            closedValveSum = openableValves.filter { !gameState.openValves.contains(it) }
+                .fold(0) { acc, valve ->
+                    val moveTime = distances[gameState.currentValve.valveId]?.get(valve.valveId)!!
+                    val minMoveTime = when (gameState.currentElephantValve) {
+                        null -> moveTime
+                        else -> min(moveTime, distances[gameState.currentElephantValve.valveId]?.get(valve.valveId)!!)
+                    }
+                    acc + (timeLeft - minMoveTime - 1) * valve.flowrate
+                }
+        }
 
- */
+        val openValveSum = timeLeft * gameState.currentPressure()
+        return gameState.totalPressureReleased + closedValveSum + openValveSum
+    }
 
-private class Cave(val valves: List<Valve>, val minutes: Int = 30, var totalPressureReleased: Int = 0) {
-    val openValves = mutableSetOf<Valve>()
-    val valvesBySymbol = valves.groupBy { it.symbol }
-    fun getValve(symbol: String): Valve = valvesBySymbol[symbol]?.first()!!
-    var currentValve = getValve("AA")
-    fun currentPressure() = openValves.map { it.flowrate }.sum()
+    fun nextStatesFromState(state: GameState): Set<GameState> {
+        val newStates = mutableSetOf<GameState>()
 
-    val initialState = GameState(currentValve = getValve("AA"), openValves = mutableSetOf())
-
-    fun nextStates(fromStates: Set<GameState>): Set<GameState> {
-        val nextStates = fromStates.flatMap { state ->
-            val newStates = mutableSetOf<GameState>()
-            // avaaminen
-            if (!state.openValves.contains(state.currentValve) && state.currentValve.flowrate > 0) {
-//                val newState = state.copy(openValves = state.openValves + state.currentValve).withNewTotalPressure()
-                val newState = state.copy().withNewTotalPressure().copy(openValves = state.openValves + state.currentValve)
+        if (state.currentElephantValve == null) {
+            // open current
+            if (state.currentValveOpenable()) {
+                val newState = state.copy(
+                    totalPressureReleased = state.totalPressureReleased + state.currentPressure(),
+                    openValves = state.openValves + state.currentValve
+                )
                 newStates.add(newState)
             }
-            // odottaminen, koska kaikki on avattu
-            if (state.openValves.size == valves.size) {
-                val newState = state.copy().withNewTotalPressure()
-                newStates.add(newState)
-            }
-            // liikkuminen
+            // move to neighbors
             newStates.addAll(state.currentValve.neighbors.map {
-                val nextValve = getValve(it)
+                val nextValve = valvesById[it]!!
                 val newState = state.copy(currentValve = nextValve).withNewTotalPressure()
                 newState
             })
-            newStates
-        }.toSet()
+        }
+        else {
+            // I move, the elephant moves
+            newStates.addAll(state.currentValve.neighbors.flatMap {
+                val nextValve = valvesById[it]!!
+                state.currentElephantValve.neighbors
+                    .map { ev -> valvesById[ev]!! }
+                    .map { eValve ->
+                        state.copy(currentValve = nextValve,
+                            currentElephantValve = eValve,
+                            totalPressureReleased = state.totalPressureReleased + state.currentPressure())
+                    }.toSet()
+            })
 
-        // prune if currentvalve and open valves are the same
+            // I move, the elephant opens
+            if (state.currentElephantValveOpenable()) {
+                newStates.addAll(state.currentValve.neighbors.map {
+                    val nextValve = valvesById[it]!!
+                    state.copy(currentValve = nextValve,
+                        openValves = state.openValves + state.currentElephantValve,
+                        totalPressureReleased = state.totalPressureReleased + state.currentPressure())
+                })
+            }
+
+            // I open, the elephant moves
+            if (state.currentValveOpenable()) {
+                newStates.addAll(state.currentElephantValve.neighbors.map {
+                    val nextValve = valvesById[it]!!
+                    state.copy(currentElephantValve = nextValve,
+                        openValves = state.openValves + state.currentValve,
+                        totalPressureReleased = state.totalPressureReleased + state.currentPressure())
+                })
+            }
+
+            // I open, the elephant opens
+            if (state.currentValveOpenable() && state.currentElephantValveOpenable()) {
+                val newState = state.copy(
+                    totalPressureReleased = state.totalPressureReleased + state.currentPressure(),
+                    openValves = state.openValves + state.currentValve + state.currentElephantValve
+                )
+                newStates.add(newState)
+            }
+        }
+
+        return newStates
+    }
+
+    fun nextStates(fromStates: Set<GameState>, timeLeft: Int): Set<GameState> {
+        val nextStates = fromStates.flatMap { state ->
+            val newStates = mutableSetOf<GameState>()
+
+            // just wait because all valves are open
+            if (state.openValves.size == openableValveCount) {
+                val newState = state.copy().withNewTotalPressure()
+                newStates.add(newState)
+            }
+            else {
+                // add all possible states, where we open valves or move
+                newStates.addAll(nextStatesFromState(state))
+            }
+
+            newStates
+        }
 
         return nextStates
-    }
-
-    private fun nextPaths(currentPaths: List<Path>, visited: MutableSet<Valve>): List<Path> {
-        val newPaths = currentPaths.map { path ->
-            val nextAvailable = path.last().neighbors.map { getValve(it) }.filter { !visited.contains(it) }
-            if (nextAvailable.isEmpty()) emptyList<Path>()
-            visited.addAll(nextAvailable)
-            nextAvailable.map { path + it }
-        }.filter { it.isNotEmpty() }.flatten()
-
-        return newPaths;
-    }
-
-    fun getShortestPathTo(fromValve: Valve, toValve: Valve): Path {
-        if (fromValve == toValve) return emptyList()
-
-        var currentPaths = listOf(listOf(fromValve))
-        val visited = mutableSetOf(fromValve)
-
-        for (steps in 1 until 1000) {
-            val nextPaths = nextPaths(currentPaths, visited)
-            val winningPaths = nextPaths.filter { toValve == it.last() }
-            if (winningPaths.isNotEmpty()) {
-                return winningPaths.first()
-            }
-            visited.addAll(nextPaths.map { it.last() })
-            currentPaths = nextPaths
-        }
-        throw IllegalArgumentException("No path from $fromValve to $toValve")
-    }
-
-    fun getReleaseSum(round: Int, distanceTo: Int, valve: Valve): Int {
-        if (valve.flowrate == 0) return 0
-        val minutesLeft = minutes - round
-        return (minutesLeft - distanceTo) * valve.flowrate
-    }
-
-    fun resolveNextValve(round: Int): Pair<Valve, Valve> {
-        // optimal valve releases the most pressure?
-        // What is the sum of pressure to end minus steps to move?
-        val nextValves = valves
-            .filter { !openValves.contains(it) && it.flowrate > 0 }
-            .map { valve -> Pair(valve, getShortestPathTo(currentValve, valve)) }
-            .map { (valve, path) -> Triple(valve, path, getReleaseSum(round, path.size - 1, valve)) }
-
-//            .map { valve -> Pair(valve, getReleaseSum(round, getShortestPathTo(currentValve, valve).size - 1, valve)) }
-
-        if (nextValves.isEmpty()) {
-            // no more valves to open
-            return Pair(currentValve, currentValve)
-        }
-
-        val (nextValve, path, releaseSum) = nextValves.maxBy { (_, _, releaseSum) -> releaseSum }
-
-        println("$nextValve, $path, $releaseSum")
-        return Pair(nextValve, if (currentValve == nextValve) currentValve else path[1])
-    }
-
-    fun playRound(round: Int) {
-        println("== Minute $round ==, you are at ${currentValve.symbol}")
-        if (openValves.isEmpty()) {
-            println("No valves are open.")
-        }
-        else {
-            val currentPressure = currentPressure()
-            totalPressureReleased += currentPressure
-            println("Valves ${openValves.map {it.symbol}.joinToString(",") } are open, releasing $currentPressure pressure.")
-        }
-
-        val (targetValve, nextValve) = resolveNextValve(round)
-        if (currentValve == targetValve) {
-            if (!openValves.contains(currentValve)) {
-                println("You open valve ${currentValve.symbol}")
-                openValves.add(currentValve)
-            }
-        }
-        else {
-            println("You move to valve to ${nextValve.symbol} from ${currentValve.symbol} ")
-        }
-
-        currentValve = nextValve
+            .sortedBy { state -> theoreticalMax(state, timeLeft) }
+            .reversed()
+            .take(1000)
+            .toSet()
     }
 }
 
-fun part1(inputLines: List<String>): Int {
-    val cave = Cave(inputLines.map { Valve.parse(it) })
+fun playRounds(inputLines: List<String>, minutes: Int, elephantValveId: ValveId?): Int {
+    val tunnels = Tunnels(inputLines.map { Valve.parse(it) }, "AA", elephantValveId)
 
-//    for (i in 1 .. 30) {
-//        cave.playRound(i)
-//    }
-
-    val expectedPath = listOf<String>(
-        "AA", "DD", "DD", "CC", "BB", "BB", "AA", "II", "JJ", "JJ", "II",
-        "AA", "DD", "EE", "FF", "GG", "HH", "HH", "GG", "FF", "EE", "EE",
-        "DD", "CC", "CC",
-        "CC", "CC", "CC", "CC", "CC", "CC",
-
-    )
-    var currentStates = setOf(cave.initialState)
-    for (i in 1 .. 30) {
-        val move = if (expectedPath[i-1] == expectedPath[i]) "open/stay ${expectedPath[i-1]}" else "move ${expectedPath[i-1]}->${expectedPath[i]}"
+    var currentStates = setOf(tunnels.initialState)
+    for (i in 1 .. minutes) {
         println("== Minute $i (${currentStates.size} states)==")
-//        println(currentStates.sortedBy { it.currentValve.symbol }.joinToString("\n"))
-        println(move)
-        currentStates = cave.nextStates(currentStates)
-//        println(currentStates.sortedBy { it.currentValve.symbol }.joinToString("\n"))
+        currentStates = tunnels.nextStates(currentStates, minutes - i)
     }
 
-
-    /*
-    == Minute 30 (138 957 states)==
-open/stay CC
-     */
-//    println(currentStates.sortedBy { it.currentValve.symbol }.joinToString("\n"))
     return currentStates.maxBy { it.totalPressureReleased }.totalPressureReleased
-}
-
-fun part2(inputLines: List<String>): Int {
-    return 0
 }
 
 fun main() {
     val inputLines = readTestInput("day16")
-    println(part1(inputLines))
-    println(part2(inputLines))
+    println(playRounds(inputLines, 30, null))
+    println(playRounds(inputLines, 26, "AA"))
 }
