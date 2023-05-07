44b3669bd02aae438988b07fd44f022f3515d6c9
Janne Rintanen <janne.rintanen@solita.fi>
2023-05-07 13:05:22 +0300

    Add solution to day 18 part 2.

--- src/main/kotlin/day18/main.kt
+++ src/main/kotlin/day18/main.kt
@@ -1,6 +1,6 @@
 package day18
 
-import util.readInput
+import util.readTestInput
 
 private data class Coord3d(val x: Int, val y: Int, val z: Int) {
     fun neighbors(): List<Coord3d> = listOf(
@@ -26,7 +26,44 @@ private fun part1(coords: List<Coord3d>): Int =
         MAX_SIDES_EXPOSED - neighborCount
     }
 
+private fun part2(coords: Set<Coord3d>): Int {
+    fun getRange(func: (Coord3d) -> Int): IntRange {
+        val values = coords.map {func(it) }
+        return values.min() - 1 .. values.max() + 1
+    }
+    // Search space is a cube around the lava:
+    val xRange = getRange { it.x }
+    val yRange = getRange { it.y }
+    val zRange = getRange { it.z }
+
+    // Create a queue and start from a corner
+    val queue = ArrayDeque<Coord3d>()
+    queue.add(Coord3d(xRange.first, yRange.first, zRange.first))
+
+    // Search for all the points of air that touch lava
+    var sideCount = 0
+    val seen = mutableSetOf<Coord3d>()
+    while (queue.isNotEmpty()) {
+        val nextCoord = queue.removeFirst()
+        if (nextCoord !in seen) {
+            seen += nextCoord
+            nextCoord.neighbors()
+                // Stay inside the search space
+                .filter { it.x in xRange && it.y in yRange && it.z in zRange }
+                .forEach { neighbor ->
+                    when (neighbor) {
+                        in coords -> sideCount += 1
+                        else -> queue.add(neighbor)
+                    }
+                }
+        }
+    }
+
+    return sideCount
+}
+
 fun main() {
-    val coords = readInput("day18").map { Coord3d.parse(it) }
-    println(part1(coords))
+    val lavaCoords = readTestInput("day18").map { Coord3d.parse(it) }
+    println(part1(lavaCoords))
+    println(part2(lavaCoords.toSet()))
 }
